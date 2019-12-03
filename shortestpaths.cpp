92f33ef866798e6123ee332d6fc8fbaef24d18c4
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2019-12-03 17:26:32 -0500

    started main method

--- shortestpaths.cpp
+++ shortestpaths.cpp
@@ -7,6 +7,37 @@
  * Pledge : I pledge my honor that I have abided by the Stevens Honor System.
  ******************************************************************************/
 
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
 int main(int argc, char* const argv[]) {
+
+    istringstream iss;
+
+    if( argc != 2 ) {
+        cerr << "Usage: ./anagramfinder <dictionary file>" << endl;
+        return 1;
+    }
+
+    //Creating a stream for the dictionary file
+    string filepath( argv[1] );
+    ifstream file;
+    file.open(filepath);
+    if( !file.is_open() ) { //File was not correctly loaded
+        cerr << "Error: File '" << filepath << "' not found." << endl;
+        return 1;
+    }
+
+    if ( !file.eof() ) {
+        string to_insert;
+        file >> to_insert;
+        while( !file.eof() ) {
+            //We do what we want with to_insert here
+
+            file >> to_insert;  //No touch this line
+        }
+    }
+
     return 0; //Everything worked fine
 }
